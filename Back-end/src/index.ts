849b4492627069756398cd99b10161747c51170f
alysalah83 <alysalah83@gmail.com>
2026-02-26 03:03:17 +0200

    adding configration for deployment on vercel

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -2,6 +2,10 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
-});
+  });
+}
+
+export default app;
