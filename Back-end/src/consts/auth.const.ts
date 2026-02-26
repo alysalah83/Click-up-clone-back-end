e822e6779398c1b2358dfb2be26ba0ebff039d7c
alysalah83 <alysalah83@gmail.com>
2026-02-26 17:52:51 +0200

    making cookie options only for production

--- Back-end/src/consts/auth.const.ts
+++ Back-end/src/consts/auth.const.ts
@@ -1,14 +1,8 @@
-export const COOKIES_OPTIONS =
-  process.env.NODE_ENV === "development"
-    ? ({
-        httpOnly: true,
-        secure: false,
-        sameSite: "lax",
-        maxAge: 90 * 24 * 60 * 60 * 1000,
-      } as const)
-    : ({
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 90 * 24 * 60 * 60 * 1000,
-      } as const);
+export const COOKIES_OPTIONS = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 90 * 24 * 60 * 60 * 1000,
+} as const;
+
+console.log(process.env.NODE_ENV);
