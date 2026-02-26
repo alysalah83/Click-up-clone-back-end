97889b534b5f3329ba00f97d2c1e6b2b1eda74af
alysalah83 <alysalah83@gmail.com>
2026-02-26 17:40:35 +0200

    editing token cookie proprties for production

--- Back-end/src/consts/auth.const.ts
+++ Back-end/src/consts/auth.const.ts
@@ -1,6 +1,14 @@
-export const COOKIES_OPTIONS = {
-  httpOnly: true,
-  secure: false,
-  sameSite: "lax",
-  maxAge: 90 * 24 * 60 * 60 * 1000,
-} as const;
+export const COOKIES_OPTIONS =
+  process.env.NODE_ENV === "production"
+    ? ({
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 90 * 24 * 60 * 60 * 1000,
+      } as const)
+    : ({
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 90 * 24 * 60 * 60 * 1000,
+      } as const);
