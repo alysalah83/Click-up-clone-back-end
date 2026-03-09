b14e19dde58dc7de108cf21d302106539f6c97db
alysalah83 <alysalah83@gmail.com>
2026-03-09 16:43:02 +0200

    adding prisma accelerate for fixing edge function problem

--- Back-end/src/lib/prisma.ts
+++ Back-end/src/lib/prisma.ts
@@ -1,10 +1,9 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL,
+}).$extends(withAccelerate());
 
 export { prisma };
